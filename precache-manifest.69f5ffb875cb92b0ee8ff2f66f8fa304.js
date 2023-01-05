814080af082c3e407d064ec88465e90771f49dd0
Ryan Farrell <rfarr@zestbloom.com>
2023-01-05 15:26:53 +0000

    Updates

---
+++ precache-manifest.69f5ffb875cb92b0ee8ff2f66f8fa304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835b710a1556ae3458e76da739c6e4e9",
+    "url": "/renft/index.html"
+  },
+  {
+    "revision": "61a7bf3ea5b2ede988c1",
+    "url": "/renft/static/css/main.a746408a.chunk.css"
+  },
+  {
+    "revision": "a311627347a55f2b6881",
+    "url": "/renft/static/js/2.bf3527a2.chunk.js"
+  },
+  {
+    "revision": "eba37c39299806757d948a491c39ba57",
+    "url": "/renft/static/js/2.bf3527a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a7bf3ea5b2ede988c1",
+    "url": "/renft/static/js/main.f5283416.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/renft/static/js/main.f5283416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b3ba129b5bbc09f42b",
+    "url": "/renft/static/js/runtime-main.c85fc652.js"
+  }
+]);
